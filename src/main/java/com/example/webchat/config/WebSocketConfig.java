65978512b4efa0777f30d96dd534607430fbab13
Oleg Dmitriev <adv1e@mail.ru>
2024-10-11 22:35:05 +0300

    refactor: WebSocketConfig to use STOMP Broker Relay with RabbitMQ

--- src/main/java/com/example/webchat/config/WebSocketConfig.java
+++ src/main/java/com/example/webchat/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.webchat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,9 +11,30 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${spring.rabbitmq.host}")
+    private String relayHost;
+
+    @Value("${spring.rabbitmq.port}")
+    private int relayPort;
+
+    @Value("${spring.rabbitmq.username}")
+    private String relayUsername;
+
+    @Value("${spring.rabbitmq.password}")
+    private String relayPassword;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic")
+                .setRelayHost(relayHost)
+                .setRelayPort(61613)  // STOMP порт
+                .setClientLogin(relayUsername)
+                .setClientPasscode(relayPassword)
+                .setSystemLogin(relayUsername)
+                .setSystemPasscode(relayPassword)
+                .setSystemHeartbeatSendInterval(5000)  // 5 секунд
+                .setSystemHeartbeatReceiveInterval(5000);  // 5 секунд
+
         config.setApplicationDestinationPrefixes("/app");
     }
 
