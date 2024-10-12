de86f0bc7df9b2d584888f3759dbbe10c268f54a
Oleg Dmitriev <adv1e@mail.ru>
2024-10-12 23:13:06 +0300

    add: MessageNotFoundException to work with Status

---
+++ src/main/java/com/example/webchat/exception/MessageNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.webchat.exception;
+
+public class MessageNotFoundException extends Exception {
+
+    public MessageNotFoundException(String message) {
+        super(message);
+    }
+}
